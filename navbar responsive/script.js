f66b171d3436623aab2df4743aea6c530fc44a45
Sarvdx <azwa.luwu24@smk.belajar.id>
2024-11-08 21:33:59 +0700

    Add files via upload

---
+++ navbar responsive/script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function () {
+  const navbarMenu = document.getElementById("navbarMenu");
+  navbarMenu.classList.toggle("show");
+});
