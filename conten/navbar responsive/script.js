c08b77792c91ebd9ef55b50d6c4ee1fcf294fccc
Sarvdx <azwa.luwu24@smk.belajar.id>
2024-11-08 21:34:15 +0700

    Delete conten/navbar responsive directory

--- conten/navbar responsive/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("hamburger").addEventListener("click", function () {
-  const navbarMenu = document.getElementById("navbarMenu");
-  navbarMenu.classList.toggle("show");
-});
